d96f8a9ea2e6d70c7f6dee864a44d4de42013ba2
Martin Scheiber <martin.scheiber@aau.at>
2023-06-06 13:15:41 +0200

    added argparser to view_frames.py for duration & file destination

--- tf2_tools/scripts/view_frames.py
+++ tf2_tools/scripts/view_frames.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-# Copyright (c) 2008, Willow Garage, Inc.
+# tf2 view frame & generate_dot code Copyright (c) 2008, Willow Garage, Inc.
+# argparser & check_file code Copyright (c) 2023, Martin Scheiber, 
+#    Control of Networked Systems Group, University of Klagenfurt, Austria
+# positive_float code Copyright (c) 2018, Lucas Walter
 # All rights reserved.
 # 
 # Redistribution and use in source and binary forms, with or without
@@ -26,8 +29,11 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-# author: Wim Meeussen
+# authors: Wim Meeussen, Martin Scheiber
 
+import argparse
+import sys
+import os
 import rospy
 import tf2_py as tf2
 import yaml
@@ -37,21 +43,38 @@ import tf2_ros
 
 def main():
     rospy.init_node('view_frames')
+
+    # parse arguments
+    other_args = rospy.myargv(argv=sys.argv)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--duration",
+                        help="duration for listening to tf data, must be > 0.0",
+                        default=5.0,
+                        type=positive_float)
+    parser.add_argument("-f", "--file",
+                        help="file to store the tf graph",
+                        default="frames",
+                        type=str)
+
+    args = parser.parse_args(other_args[1:]) # Remove first arg
+    file = check_file(args.file)
     
     # listen to tf for 5 seconds
-    rospy.loginfo('Listening to tf data during 5 seconds...')
+    rospy.loginfo('Listening to tf data during %.1f seconds...' % args.duration)
     rospy.sleep(0.00001)
     buffer = tf2_ros.Buffer()
     listener = tf2_ros.TransformListener(buffer)
-    rospy.sleep(5.0)
+    rospy.sleep(args.duration)
 
-    rospy.loginfo('Generating graph in frames.pdf file...')
+    # check file
+    rospy.loginfo('Generating graph in %s.pdf file...' % file)
     rospy.wait_for_service('~tf2_frames')
     srv = rospy.ServiceProxy('~tf2_frames', FrameGraph)
     data = yaml.safe_load(srv().frame_yaml)
-    with open('frames.gv', 'w') as f:
+    with open(file + '.gv', 'w') as f:
         f.write(generate_dot(data))
-    subprocess.Popen('dot -Tpdf frames.gv -o frames.pdf'.split(' ')).communicate()
+    cmd = 'dot -Tpdf ' + file + '.gv -o ' + file + '.pdf'
+    subprocess.Popen(cmd.split(' ')).communicate()
 
 def generate_dot(data):
     if len(data) == 0:
@@ -76,6 +99,26 @@ def generate_dot(data):
     dot += '}->"'+root+'";\n}'
     return dot
 
+def check_file(file):
+    # check if file ends with '.pdf'
+    if file.endswith('.pdf'):
+        file = file[:-4] # remove format, will be added seperatly
+    
+    # check if path to file exists
+    fparts = file.split('/')
+    path = '/'.join(fparts[:-1])
+    if not os.path.exists(path):
+        print('%s does not exist, creating it' % path)
+        os.makedirs(path)
+    
+    return file
+
+def positive_float(x):
+    """temporaray import from https://github.com/ros/geometry2/blob/bd06488f/tf2_tools/scripts/echo.py#L201-205 for internal use only"""
+    x = float(x)
+    if x <= 0.0:
+        raise argparse.ArgumentTypeError("{} must be > 0.0".format(x))
+    return x
 
 if __name__ == '__main__':
     main()
